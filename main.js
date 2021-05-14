3b711edb5b13a399eccddd0363debf994536c8c3
mustafaha1 <61194773+mustafaha1@users.noreply.github.com>
2021-05-14 17:05:22 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -4,69 +4,167 @@ let products = [
     {
         name: "peproni",
         size: 12,
-        price: "£" + 7.50,
-        inCart: 0  
+        tag: "peproni",
+        price: 7.50,
+        inCart: 0
     },
 
     {
         name: "special",
         size: 12,
-        price: "£" + 7,
-        inCart: 0  
+        tag: "special",
+        price: 7,
+        inCart: 0
+    },
+
+    {
+        name: "mightymeaty",
+        size: 12,
+        tag: "mightymeaty",
+        price: 7,
+        inCart: 0
     },
 
     {
         name: "special",
         size: 12,
-        price: "£" + 7,
-        inCart: 0  
+        tag: "hawaian",
+        price: 7,
+        inCart: 0
     },
 
     {
         name: "special",
         size: 12,
-        price: "£" + 7,
-        inCart: 0  
-    },
-
-    {
-        name: "special",
-        size: 12,
-        price: "£" + 7,
-        inCart: 0  
+        tag: "cheese& tomato",
+        price: 7,
+        inCart: 0
     }
-]
+];
 
 
-for(i = 0; i < btn.length; i++){
-    btn[i].addEventListener('click', function(){
-        cartNumbers();
- })
+for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener('click', () => {
+        cartNumbers(products[i]);
+        totalCost(products[i]);
+    })
 }
 
 
-function onLoadCartNumber (){
-    let productNumber = localStorage.getItem('cartNumbers');
+function onLoadCartNumbers() {
+    let productNumbers = localStorage.getItem('cartNumbers');
 
-    if (productNumber){
-        document.querySelector('.nav-item span').textContent = productNumber;
+    if (productNumbers) {
+        document.querySelector('.nav-item span').textContent = productNumbers;
     }
 }
 
-function cartNumbers(){
-    let productNumber = localStorage.getItem('cartNumbers');
+function cartNumbers(product) {
+    let productNumbers = localStorage.getItem('cartNumbers');
 
-    productNumber = parseInt(productNumber);
+    productNumbers = parseInt(productNumbers);
 
-    if(productNumber){
-        localStorage.setItem("cartNumbers",  productNumber + 1);
-        document.querySelector('.nav-item span').textContent = productNumber + 1;
+    if (productNumbers) {
+        localStorage.setItem("cartNumbers", productNumbers + 1);
+        document.querySelector('.nav-item span').textContent = productNumbers + 1;
     } else {
         localStorage.setItem("cartNumbers", 1);
         document.querySelector('.nav-item span').textContent = 1;
     }
-    
+
+    setItems(product);
+}
+
+function setItems(product) {
+    let cartItems = localStorage.getItem("productsinCart");
+    cartItems = JSON.parse(cartItems);
+    if (cartItems != null) {
+
+        if (cartItems[product.tag] == undefined) {
+            cartItems = {
+                ...cartItems,
+                [product.tag]: product
+            }
+
+        }
+        cartItems[product.tag].inCart += 1;
+    } else {
+        product.inCart = 1;
+        cartItems = {
+            [product.tag]: product
+        }
+    }
+    localStorage.setItem("productsinCart", JSON.stringify(cartItems));
+}
+
+function totalCost(product) {
+
+
+    let cartCost = localStorage.getItem("totalCost");
+
+
+    if (cartCost != null) {
+        cartCost = parseInt(cartCost);
+        localStorage.setItem("totalCost", cartCost + product.price);
+    } else {
+        localStorage.setItem("totalCost", product.price);
+    }
+
+
+
 
 }
 
-onLoadCartNumber ();
+
+function displayCart() {
+    let cartItems = localStorage.getItem("productsinCart");
+    cartItems = JSON.parse(cartItems);
+    let productContainer = document.querySelector(".products");
+    let cartCost = localStorage.getItem("totalCost");
+
+    if (cartItems && productContainer) {
+
+        productContainer.innerHTML = '';
+        Object.values(cartItems).map(item => {
+
+            productContainer.innerHTML += `
+            <div class='products'>
+            <ion-icon name="close-outline"></ion-icon>
+                //  <img src="img/${item.tag}.jpg">
+                   <span>${item.size}</span>
+                   <span>${item.name}</span>
+            </div>
+            <div class="price">£${item.price},0</div>
+
+            <div class="quantity>
+               <ion-icon name="caret-up-outline"></ion-icon>
+               <span>${item.inCart}</span
+               <ion-icon name="caret-down-outline"></ion-icon>
+            </div>
+
+            <div class="total">
+            £${item.inCart * item.price},0
+            </div>
+            `;
+
+
+        });
+
+        productContainer.innerHTML +=
+            `
+        <div class="basketTotalContainer">
+        <h4 class="basketTotalTitle"> Basket Total </h4>
+        <h4 class ="basketTotal"> 
+            £${cartCost},00
+        </h4> 
+        </div>
+
+        `;
+
+
+    }
+}
+
+
+onLoadCartNumbers();
+displayCart();
